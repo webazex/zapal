c4b7719ca048865930c2dcdc25e6d81dd8f5da22
webazex <webazex@te.net.ua>
2026-09-23 18:38:42 +0300

    fix enqueued assets

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,7 +1,10 @@
 <?php
 
-function zapal_get_sources() {
+function zapal_enqueue_assets() {
     wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
-    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/js/zapal.js', array( 'jquery' ) );
+    get_template_directory_uri() . '/js/main.js'
+        |> get_theme_file_path(...)
+        |> (fn($x) => filemtime($x, ['in_footer' => true]))
+        |> (fn($x) => wp_enqueue_script('zapal-js', get_template_directory_uri() . '/js/main.js', ['jquery'], $x));
 }
-add_action('wp_enqueue_scripts', 'zapal_get_sources');
+add_action('wp_enqueue_scripts', 'zapal_enqueue_assets');
