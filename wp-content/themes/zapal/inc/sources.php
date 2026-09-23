636dacda518182491140a0e0ac353e9efb618c00
webazex <webazex@te.net.ua>
2026-09-23 18:41:32 +0300

    fix syntax enqueued assets

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,10 +1,10 @@
 <?php
 
-function zapal_enqueue_assets() {
+function zapal_get_sources() {
     wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
-    get_template_directory_uri() . '/js/main.js'
-        |> get_theme_file_path(...)
-        |> (fn($x) => filemtime($x, ['in_footer' => true]))
-        |> (fn($x) => wp_enqueue_script('zapal-js', get_template_directory_uri() . '/js/main.js', ['jquery'], $x));
+    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/js/main.js',
+        ['jquery'],
+        filemtime( get_template_directory_uri() . '/js/main.js'),
+        ['in_footer' => true] );
 }
-add_action('wp_enqueue_scripts', 'zapal_enqueue_assets');
+add_action('wp_enqueue_scripts', 'zapal_get_sources');
