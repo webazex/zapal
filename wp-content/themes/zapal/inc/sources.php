56f458b3c3a9ca32030f3c5b6fdbc7fcbd726a34
webazex <webazex@te.net.ua>
2026-09-23 18:57:54 +0300

    fix sources assets patch

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,10 +1,12 @@
 <?php
 
 function zapal_get_sources() {
+    $jsPath = ASSET_DIR . 'js' . DIRECTORY_SEPARATOR . 'main.js';
+    $jsUri  = ASSET_DIR_URI . 'js/main.js';
     wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
-    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/assets/js/main.js',
+    wp_enqueue_script( 'zapal-js', $jsUri,
         ['jquery'],
-        filemtime( get_template_directory() . '/assets/js/main.js'),
+        filemtime( $jsPath),
         ['in_footer' => true] );
 }
 add_action('wp_enqueue_scripts', 'zapal_get_sources');
