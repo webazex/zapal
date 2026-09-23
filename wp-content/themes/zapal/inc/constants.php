7232af219a90885451a2e4ba5e4f42ae0e8c98b4
webazex <webazex@te.net.ua>
2026-09-23 18:34:40 +0300

    fix theme infrastructure

--- wp-content/themes/zapal/inc/constants.php
+++ wp-content/themes/zapal/inc/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('ASSET_DIR', get_template_directory_uri() . DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR);
+define('ASSET_DIR', get_template_directory() . DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR);
+define('ASSET_DIR_URI', get_template_directory_uri().'/assets/');
+
 define('INC_DIR', get_template_directory() . DIRECTORY_SEPARATOR.'inc'.DIRECTORY_SEPARATOR);
-define("CORE_DIR", get_template_directory() . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR);
-const CORE_CLASSES_DIR = CORE_DIR . 'classes' . DIRECTORY_SEPARATOR;
--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,8 +1,7 @@
 <?php
-use WBZX\zapal\core\assets\WBZX_Asset as Asset;
+
 function zapal_get_sources() {
-    require_once CORE_CLASSES_DIR.'WBZX_Asset.php';
-    Asset::css('main', get_stylesheet_uri());
-    Asset::js('main', ASSET_DIR.'js/main.js', ['jquery'], false, ['in_footer' => true]);
+    wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
+    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/js/zapal.js', array( 'jquery' ) );
 }
 add_action('wp_enqueue_scripts', 'zapal_get_sources');
