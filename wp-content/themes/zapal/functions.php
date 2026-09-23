301e9890574bf5def5a80dc0bdff0e7a8235dd81
webazex <webazex@te.net.ua>
2026-09-23 18:59:39 +0300

    fck2

--- wp-content/themes/zapal/functions.php
+++ wp-content/themes/zapal/functions.php
@@ -1,6 +1,6 @@
 <?php
 //load zapal constants
-require_once get_template_directory().DIRECTORY_SEPARATOR.'inc'.DIRECTORY_SEPARATOR.'constants.php';;
+require_once get_template_directory().DIRECTORY_SEPARATOR.'inc'.DIRECTORY_SEPARATOR.'constants.php';
 
 //add css&js
 require_once INC_DIR.'sources.php';
