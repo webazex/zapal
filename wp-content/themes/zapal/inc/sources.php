564a5ee7f136d0e00260ded8b9955d37ad3a20e3
webazex <webazex@te.net.ua>
2026-09-23 18:58:46 +0300

    fck

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,6 +1,6 @@
 <?php
 
-function zapal_get_sources() {
+function zapal_enqueue_sources() {
     $jsPath = ASSET_DIR . 'js' . DIRECTORY_SEPARATOR . 'main.js';
     $jsUri  = ASSET_DIR_URI . 'js/main.js';
     wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
@@ -9,4 +9,4 @@ function zapal_get_sources() {
         filemtime( $jsPath),
         ['in_footer' => true] );
 }
-add_action('wp_enqueue_scripts', 'zapal_get_sources');
+add_action('wp_enqueue_scripts', 'zapal_enqueue_sources');
