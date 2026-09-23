42d4a53df474fcad3b2af036571a68870da1fc80
webazex <webazex@te.net.ua>
2026-09-23 18:50:06 +0300

    fix filetime patch

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -2,9 +2,9 @@
 
 function zapal_get_sources() {
     wp_enqueue_style( 'zapal-css', get_stylesheet_uri() );
-    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/js/main.js',
+    wp_enqueue_script( 'zapal-js', get_template_directory_uri() . '/assets/js/main.js',
         ['jquery'],
-        filemtime( get_template_directory_uri() . '/js/main.js'),
+        filemtime( get_template_directory() . '/assets/js/main.js'),
         ['in_footer' => true] );
 }
 add_action('wp_enqueue_scripts', 'zapal_get_sources');
