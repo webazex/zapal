7d8063c78958a55f42535828cb436b17364f70f7
webazex <webazex@te.net.ua>
2026-09-23 19:19:24 +0300

    replace core jquery in footer

--- wp-content/themes/zapal/inc/sources.php
+++ wp-content/themes/zapal/inc/sources.php
@@ -1,5 +1,19 @@
 <?php
+add_action('wp_default_scripts', function ($scripts) {
+    if (is_admin()) {
+        return;
+    }
 
+    if (isset($scripts->registered['jquery'])) {
+        $scripts->registered['jquery']->deps = array_diff(
+            $scripts->registered['jquery']->deps,
+            ['jquery-migrate']
+        );
+    }
+
+    $scripts->add_data('jquery', 'group', 1);
+    $scripts->add_data('jquery-core', 'group', 1);
+});
 function zapal_enqueue_sources() {
     $jsPath = ASSET_DIR . 'js' . DIRECTORY_SEPARATOR . 'main.js';
     $jsUri  = ASSET_DIR_URI . 'js/main.js';
